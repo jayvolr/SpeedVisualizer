4f9e715da831ddd22e75f7ce9b77d5f85a020ed2
Jon Love <hi@jlov.io>
2024-11-02 15:53:31 -0400

    add suggested subjects

--- src/lib/SmartInput.svelte
+++ src/lib/SmartInput.svelte
@@ -13,6 +13,18 @@
   let loading: boolean = $state(false)
   let lastRun: string = ''
 
+  const subjects = [
+      'Car', 'Bike', 'Person', 'Plane', 'Train', 'Boat', 'Fighter Jet', 'Rocket', 'Bullet', 'Sound', 'Light',
+      'Usain Bolt', 'Cheetah', 'Sonic', 'The Flash', 'Speedy Gonzales', 'Road Runner', 'Speed Racer', 'Speedster',
+      'Bullet Train', 'Concorde', 'SR-71 Blackbird', 'F-22 Raptor', 'F-35 Lightning II', 'Peregrine Falcon',
+      'Formula 1 Car', 'Bugatti Veyron', 'Koenigsegg Agera RS', 'Hennessey Venom GT', 'Bloodhound SSC', 'Voyager 1',
+      'New Horizons', 'Apollo 11', 'Space Shuttle', 'ISS', 'Starship', 'Enterprise', 'Millennium Falcon', 'TIE Fighter',
+      'X-Wing', 'Death Star', 'Star Destroyer', 'Borg Cube', 'Warp Drive', 'Hyperdrive', 'Earth\'s Orbit', 'Expansion of The Universe',
+      'Goku', 'Superman', 'Thor', 'Hulk', 'Quicksilver', 'Zoom', 'Reverse Flash', 'Savitar', 'Godspeed', 'Black Flash',
+      'Sonic the Hedgehog', 'Shadow the Hedgehog', 'Silver the Hedgehog', 'Metal Sonic', 'Amy Rose', 'Tails', 'Knuckles',
+      'U-2', 'Land Speed Record', 'Mach 1', 'Mach 2', 'Mach 3', 'Mach 4', 'Mach 5', 'Mach 6', 'Mach 7', 'Mach 8', 'Mach 9', 'Mach 10',
+    ]
+
   async function changed() {
     if (!subject) return
     if (subject === lastRun) return
@@ -51,25 +63,29 @@
     (event.target as HTMLInputElement).select()
   }
 
-  function getExample(): string {
-    const examples = [
-      'Car', 'Bike', 'Person', 'Plane', 'Train', 'Boat', 'Fighter Jet', 'Rocket', 'Bullet', 'Sound', 'Light',
-      'Usain Bolt', 'Cheetah', 'Sonic', 'The Flash', 'Speedy Gonzales', 'Road Runner', 'Speed Racer', 'Speedster',
-      'Bullet Train', 'Concorde', 'SR-71 Blackbird', 'F-22 Raptor', 'F-35 Lightning II', 'Peregrine Falcon',
-      'Formula 1 Car', 'Bugatti Veyron', 'Koenigsegg Agera RS', 'Hennessey Venom GT', 'Bloodhound SSC', 'Voyager 1',
-      'New Horizons', 'Apollo 11', 'Space Shuttle', 'ISS', 'Starship', 'Enterprise', 'Millennium Falcon', 'TIE Fighter',
-      'X-Wing', 'Death Star', 'Star Destroyer', 'Borg Cube', 'Warp Drive', 'Hyperdrive', 'Earth\'s Orbit', 'Expansion of The Universe',
-      'Goku', 'Superman', 'Thor', 'Hulk', 'Quicksilver', 'Zoom', 'Reverse Flash', 'Savitar', 'Godspeed', 'Black Flash',
-      'Sonic the Hedgehog', 'Shadow the Hedgehog', 'Silver the Hedgehog', 'Metal Sonic', 'Amy Rose', 'Tails', 'Knuckles',
-      'U-2', 'Land Speed Record', 'Mach 1', 'Mach 2', 'Mach 3', 'Mach 4', 'Mach 5', 'Mach 6', 'Mach 7', 'Mach 8', 'Mach 9', 'Mach 10',
-    ]
-    return examples[Math.floor(Math.random() * examples.length)]
+  function getRandomSubject(): string {
+    return subjects[Math.floor(Math.random() * subjects.length)]
   }
 </script>
 
 <div class="container">
   <span class="id" aria-hidden="true">{id}</span>
-  <input type="text" onfocus={selectAll} bind:value={subject} onblur={changed} onkeyup={maybeRemoveFocus} placeholder={`Subject e.g. "${getExample()}"`} use:pullFocus />
+
+  <input
+    type="text" bind:value={subject}
+    onfocus={selectAll} onblur={changed} onkeyup={maybeRemoveFocus}
+    placeholder={`Subject e.g. "${getRandomSubject()}"`} use:pullFocus
+    list={`subjects-${id}`}
+  />
+
+  {#if subject.length > 2}
+    <datalist id={`subjects-${id}`}>
+      {#each subjects as subject}
+        <option value={subject}>{subject}</option>
+      {/each}
+    </datalist>
+  {/if}
+  
   {#if loading}
     <span class="loader-container"><Pulse color="#dbdbdb" size="30" /></span>
   {:else}
